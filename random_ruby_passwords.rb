017af8fd92ef8bbb9a7b0d3b6e98a47d4d51c412
jbsmith86 <joel@trosic.com>
2014-09-17 12:15:57 -0700

    added class and default chracter length

--- random_ruby_passwords.rb
+++ random_ruby_passwords.rb
@@ -1,23 +1,27 @@
-def getRandByte
-  random = File.new("/dev/random", 'r')
-  random.read(1).ord
-end
+class RandomPassword
 
-def getPassword(numchar, special=true, exclude=[])
-  newpassword = ''
-  charset = (('a'..'z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a).join("")
-  if special
-    charset += '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
+  def self.get_byte
+    random = File.new("/dev/random", 'r')
+    random.read(1).ord
   end
-  exclude.each do |letter|
-    charset.replace(letter,"")
-  end
-  numchar.times do
-    rand_num = getRandByte
-    while rand_num >= charset.length
-      rand_num -= charset.length
+
+  def generate(numchar=8, special=true, exclude=[])
+    newpassword = ''
+    charset = (('a'..'z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a).join("")
+    if special
+      charset += '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
     end
-    newpassword += charset[rand_num]
+    exclude.each do |letter|
+      charset.replace(letter,"")
+    end
+    numchar.times do
+      rand_num = self.get_byte
+      while rand_num >= charset.length
+        rand_num -= charset.length
+      end
+      newpassword += charset[rand_num]
+    end
+    return newpassword
   end
-  return newpassword
+
 end
